5b150613410ddc64a61690f232ec61751744fa41
Rafael Souza <rafaelrs57@hotmail.com>
2020-01-31 14:30:01 -0300

    Working on Ch 03

--- dec_bound.py
+++ dec_bound.py
@@ -5,7 +5,7 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_decision_regions(X, y, classifier, resolution=0.02):
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -22,12 +22,18 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
     
-    # plot class examples
     for idx, cl in enumerate(np.unique(y)):
-         plt.scatter(x=X[y == cl, 0],
-                     y=X[y == cl, 1],
-                     alpha=0.8,
-                     c=colors[idx],
-                     marker=markers[idx],
-                     label=cl,
-                     edgecolor='black')
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=cl,
+                    edgecolor='black')
+    
+    # highlight test examples
+    if test_idx:
+        # plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        
+        plt.scatter(X_test[:, 0], X_test[:, 1],
+                    c='', edgecolor='black', alpha=1.0,
+                    linewidth=1, marker='o',
+                    s=100, label='test set')
